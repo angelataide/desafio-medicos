7d2a2b765023a12cebfec642f1ff1cb40f642790
Angela Ataide <angel.santosdev@gmail.com>
2025-12-11 11:47:36 -0300

    Update App.jsx

--- medicos-dentistas/src/App.jsx
+++ medicos-dentistas/src/App.jsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Footer from './
 import Home from './pages/Home/Home';
 
-// ATENÇÃO AQUI: Caminho novo e nome novo
 import Voluntaria from './pages/Voluntaria/Voluntaria'; 
 
 function App() {
